a5cf02e30a4ce557c2a490bc37bfe64c434aabf7
A1exsandro <alexsandro.acp@gmail.com>
2022-12-10 22:58:04 -0400

    add and update image resources

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,29 +13,20 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
-
-        val countUpButton: Button = findViewById(R.id.countUp_button)
-        countUpButton.setOnClickListener { countUp() }
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        val randomInt = (1..6).random()
-        resultText.text = randomInt.toString()
-    }
-
-    private fun countUp() {
-        val resultText: TextView = findViewById(R.id.result_text)
-
-        if (resultText.text == "Hello World!") {
-            resultText.text = "1"
-        } else {
-            var resultInt = resultText.text.toString().toInt()
-
-            if (resultInt < 6) {
-                resultInt++
-                resultText.text = resultInt.toString()
-            }
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when ((1..6).random()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+
+        diceImage.setImageResource(drawableResource)
+
     }
 }
