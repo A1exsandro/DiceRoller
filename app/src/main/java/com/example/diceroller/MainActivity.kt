787d28a367ada4db8fc7a906b6f7b8b8be464a79
A1exsandro <alexsandro.acp@gmail.com>
2022-12-13 21:31:52 -0400

    change image independent

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -7,18 +7,27 @@ import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     lateinit var diceImage : ImageView
+    lateinit var diceImageTwo : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         diceImage = findViewById(R.id.dice_image)
+        diceImageTwo = findViewById(R.id.dice_image_two)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
     }
 
     private fun rollDice() {
-        val drawableResource = when ((1..6).random()) {
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImageTwo.setImageResource(getRandomDiceImage())
+    }
+
+    private fun getRandomDiceImage() : Int {
+        val randomInt = (1..6).random()
+
+        return when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -26,7 +35,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-
-        diceImage.setImageResource(drawableResource)
     }
+
 }
